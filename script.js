f6ade29396d5b54545d07290be426adbc3b007d4
HuKairs <85752239+HuKairs@users.noreply.github.com>
2024-07-23 14:44:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.querySelectorAll('.text-item').forEach(item => {
     item.addEventListener('mousemove', (e) => {
         const rect = item.getBoundingClientRect();
-        const offsetX = (e.clientX - rect.left) / rect.width * 20 - 10;
-        const offsetY = (e.clientY - rect.top) / rect.height * 20 - 10;
+        const offsetX = (e.clientX - rect.left) / rect.width * 10 - 5;
+        const offsetY = (e.clientY - rect.top) / rect.height * 10 - 5;
 
-        item.style.transform = `translate3d(${offsetX}px, ${offsetY}px, 0) rotateX(${offsetY}deg) rotateY(${offsetX}deg)`;
+        item.style.transform = `translate3d(${offsetX}px, ${offsetY}px, 0)`;
     });
 
     item.addEventListener('mouseleave', () => {
